c7d3f5e80dbfdb9e01b9eb1b179defdc17642236
Iris <54792944+Iris-666@users.noreply.github.com>
2019-12-05 17:29:34 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ let wolfclick = false;
 let trollclick = false;
 let nextbt = document.getElementById('nextbt')
 let drawTrollVideo = document.getElementById('drawTrollVideo')
+let twigintro = document.getElementById('twigintro')
+let hildaintro = document.getElementById('hildaintro')
+let wolfintro = document.getElementById('wolfintro')
+let trollintro = document.getElementById('trollintro')
+let returnbt = document.getElementById('returnbt')
+let counter=0;
+let counterall = 0;
+let hildaescape = document.getElementById('hildaescape');
+let startEscape = false;
+let escapeSuccess = document.getElementById('escapeSuccess')
+
+
 
 intervalID = setInterval(() => {
   if(index >= 8){
@@ -93,37 +105,74 @@ wolfdiv.addEventListener('mouseout',function(){
   wolfimg.style.left = '200px';
 })
 hildadiv.addEventListener('click',function(){
-  console.log('hilda intro')
-  hildaclick = true;
-  if(hildaclick == true && twigclick == true && wolfclick == true && trollclick == true){
-    nextbt.style.display = 'block';
-    console.log('next')
-  }
+  hildaintro.style.display = 'block'
+  returnbt.style.display = 'block'
+  wolfimg.style.display = 'none'
+  hildaimg.style.display = 'none'
+  trollimg.style.display = 'none'
+  twigimg.style.display = 'none'
+  startimg.style.display = 'none'
+  hildaintro.play()
 })
 twigdiv.addEventListener('click',function(){
   console.log('twig intro')
-  twigclick = true;
-  if(hildaclick == true && twigclick == true && wolfclick == true && trollclick == true){
-    nextbt.style.display = 'block';
-    console.log('next')
-  }
+  // twigclick = true;
+  twigintro.style.display = 'block'
+  returnbt.style.display = 'block'
+  wolfimg.style.display = 'none'
+  hildaimg.style.display = 'none'
+  trollimg.style.display = 'none'
+  twigimg.style.display = 'none'
+  startimg.style.display = 'none'
+  twigintro.play()
 })
 wolfdiv.addEventListener('click',function(){
-  console.log('wolf intro')
-  wolfclick = true;
-  if(hildaclick == true && twigclick == true && wolfclick == true && trollclick == true){
-    nextbt.style.display = 'block';
-    console.log('next')
-  }
+  wolfintro.style.display = 'block'
+  returnbt.style.display = 'block'
+  wolfimg.style.display = 'none'
+  hildaimg.style.display = 'none'
+  trollimg.style.display = 'none'
+  twigimg.style.display = 'none'
+  startimg.style.display = 'none'
+  wolfintro.play()
 })
 trolldiv.addEventListener("click",function(){
-  console.log('troll intro');
-  trollclick = true;
-  if(hildaclick == true && twigclick == true && wolfclick == true && trollclick == true){
-    nextbt.style.display = 'block';
-    console.log('next')
-  }
+
+  trollintro.style.display = 'block'
+  returnbt.style.display = 'block'
+  wolfimg.style.display = 'none'
+  hildaimg.style.display = 'none'
+  trollimg.style.display = 'none'
+  twigimg.style.display = 'none'
+  startimg.style.display = 'none'
+  trollintro.play()
 })
+
+let returncount = 0
+function returnToInto(){
+  twigintro.pause();
+  hildaintro.pause();
+  trollintro.pause();
+  wolfintro.pause();
+  twigintro.style.display = 'none'
+  hildaintro.style.display = 'none'
+  trollintro.style.display = 'none'
+  wolfintro.style.display = 'none'
+
+  startimg.style.display = 'block'
+  wolfimg.style.display = 'block'
+  hildaimg.style.display = 'block'
+  trollimg.style.display = 'block'
+  twigimg.style.display = 'block'
+  returnbt.style.display = 'none'
+  returncount += 1
+
+  if(returncount == 4){
+    nextbt.style.display = 'block';
+  }
+
+}
+returnbt.addEventListener('click',returnToInto)
 
 function drawTroll(){
   drawTrollVideo.style.display = 'block'
@@ -132,12 +181,63 @@ function drawTroll(){
   hildaimg.remove();
   twigimg.remove();
   trollimg.remove();
+  trolldiv.remove();
+  hildadiv.remove();
+  twigdiv.remove();
+  wolfdiv.remove();
   drawTrollVideo.play();
   nextbt.style.display = 'none';
-
 }
 
 nextbt.addEventListener('click',drawTroll);
+
+drawTrollVideo.addEventListener('ended',function(){
+  drawTrollVideo.remove()
+  hildaescape.style.display = 'block'
+  // hildaescape.play()
+  document.addEventListener('keypress',whichkey);
+})
+
+let prevkey =[]
+let playrate = []
+let keyispressed = false;
+function whichkey(){
+  var key = event.key;
+  prevkey[0] = prevkey[1]
+  prevkey[1] = key
+  console.log(prevkey)
+  if(prevkey[0] == 'k' && prevkey[1] == 'l'){
+    counter += 1;
+    counterall += 1
+    hildaescape.play();
+    
+  }
+  }
+
+
+
+  setInterval(() => {
+    hildaescape.playbackRate = counter/3;
+    playrate.push(hildaescape.playbackRate);
+    // console.log(playrate)
+    counter = 0
+  }, 1000);
+
+  hildaescape.addEventListener('ended',function(){
+    hildaescape.pause()
+    if (counterall > 40){
+      console.log('success')
+      hildaescape.pause();
+      hildaescape.remove();
+      escapeSuccess.style.display = 'block'
+      escapeSuccess.play();
+
+    }else{
+      console.log('fail')
+      
+    }
+  })
+
 
 
 
